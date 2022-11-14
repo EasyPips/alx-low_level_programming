8ab450b4688ff7195797684a5a966527cb8ed415
EasyPips <sundaybukunmi62@gmail.com>
2022-11-14 09:49:19 -0800

    h

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ *main - entry point
+ *
+ *Description: prints the file input into the preprocessor
+ *
+ *Return: 0
+ */
+int main(void)
+{
+	printf("%s\n", __FILE__);
+
+	return (0);
+}
+
